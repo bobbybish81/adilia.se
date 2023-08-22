335442aadf50024bdaa0c4042b7fa3a8231735f8
bobbybish81 <bobbybish81@hotmail.com>
2023-08-22 15:06:38 +0200

    email form and sitemap implemented

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,31 +1,34 @@
 import { useRef, useState, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import services from '../utils/services';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import '../styles/Contact.css';
 
 const ContactForm = () => {
 
   const form = useRef<HTMLFormElement>(null!);
-  const [selectedValue, setSelectedValue] = useState('')
+  const [selectedValue, setSelectedValue] = useState('');
 
-  // const sendEmail = (event:React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   const service = process.env.REACT_APP_SERVICE || '';
-  //   const template = process.env.REACT_APP_TEMPLATE || '';
-  //   const publicKey = process.env.REACT_APP_PUBLICKEY || '';
-  //   emailjs.sendForm(
-  //     service,
-  //     template,
-  //     form.current,
-  //     publicKey
-  //     ).then((result) => {
-  //         console.log(result.text);
-  //     }, (error) => {
-  //         console.log(error.text);
-  //     });
-  //     alert('meddelande skickat till adilia.se')
-  //     navigate('/');
-  // };
+  const navigate = useNavigate();
+
+  const sendEmail = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const service = process.env.REACT_APP_SERVICE || '';
+    const template = process.env.REACT_APP_TEMPLATE || '';
+    const publicKey = process.env.REACT_APP_PUBLICKEY || '';
+    emailjs.sendForm(
+      service,
+      template,
+      form.current,
+      publicKey
+      ).then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      alert('meddelande skickat till adilia.se')
+      navigate('/');
+  };
 
   const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>): void => {
     setSelectedValue(event.target.value);
@@ -34,7 +37,7 @@ const ContactForm = () => {
   return (
       <form 
         className='contact-form'
-        // onSubmit={sendEmail}
+        onSubmit={sendEmail}
         ref={form}>
         <h4>Kontaktformulär</h4>
         <input
